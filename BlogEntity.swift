fee5e5c146d8221712f2d3132d6e1a8d84e6cc5a
李嘉 <achievejia@163.com>
2015-12-21 14:48:52 +0800

    Add Entity

---
+++ BlogEntity.swift
@@ -0,0 +1,9 @@
+//
+//  BlogEntity.swift
+//  SwiftWork
+//
+//  Created by 李嘉 on 15/12/21.
+//  Copyright © 2015年 李嘉. All rights reserved.
+//
+
+import Foundation
---
+++ BrolegroupEntity.swift
@@ -0,0 +1,9 @@
+//
+//  BrolegroupEntity.swift
+//  SwiftWork
+//
+//  Created by 李嘉 on 15/12/21.
+//  Copyright © 2015年 李嘉. All rights reserved.
+//
+
+import Foundation
